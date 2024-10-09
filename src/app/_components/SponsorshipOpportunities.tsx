fe38cd1163dc8d99a78d40397ce4e822070ef62b
Emmanuel <emmanueldavidson524@gmail.com>
2024-10-09 11:04:02 +0100

    Changes made again

--- src/app/_components/SponsorshipOpportunities.tsx
+++ src/app/_components/SponsorshipOpportunities.tsx
@@ -8,7 +8,7 @@ import { fadeIn } from '../../../lib/variants';
 function SponsorshipOpportunities() {
   return (
     <div className='h-fit py-36 w-full items-center justify-center bg-black'>
-        <div className='flex max-lg:flex-col max-lg:gap-6 w-[80%] max-lg:w-[85%] justify-between mx-auto'>
+        <div className='flex h-[800px] max-lg:flex-col max-lg:gap-6 w-[80%] max-lg:w-[85%] justify-between mx-auto'>
             
             <motion.div 
                 variants={fadeIn("right", 0.2)}
@@ -24,7 +24,7 @@ function SponsorshipOpportunities() {
                 initial="hidden"
                 whileInView={"show"}
                 viewport={{once:false, amount:0.3}}
-                className='w-[48%] h-fit max-lg:w-full flex flex-col gap-4 justify-center '>
+                className='w-[48%] h-full max-lg:w-full flex flex-col gap-4 justify-center items-center'>
                 <h2 className='text-4xl font-bold tracking-widest text-red-500 uppercase max-lg:text-xl'>Sponsorship & Partnership Opportunities</h2>
                 <p className='leading-8 tracking-widest text-lg text-white max-lg:text-sm max-lg:leading-8 '>
                     Align your brand with the largest tech fair in Osun State 
